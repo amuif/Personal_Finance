3177bbf26459ce805fcf3b9d505029a94d51e703
amuif <amudihayat@gmail.com>
2025-06-26 22:59:41 +0300

    formatting code

--- src/store/user-store.ts
+++ src/store/user-store.ts
@@ -1,19 +1,19 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
-type User= {
-  name: string
-  image: string
-}
+type User = {
+  name: string;
+  image: string;
+};
 
 type Action = {
-  updateName: (firstName: User['name']) => void
-  updateImage: (lastName: User['image']) => void
-}
+  updateName: (firstName: User['name']) => void;
+  updateImage: (lastName: User['image']) => void;
+};
 
 const useUser = create<User & Action>((set) => ({
   name: '',
   image: '',
-  updateName: (newName:string) => set(() => ({ name: newName })),
-  updateImage: (newImage:string) => set(() => ({ image: newImage })),
+  updateName: (newName: string) => set(() => ({ name: newName })),
+  updateImage: (newImage: string) => set(() => ({ image: newImage })),
 }));
-export default useUser
+export default useUser;
